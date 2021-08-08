6dd38a414db9424c958c025ff4c5e08a8421ad9d
Nilaybiswa <80632166+Nilaybiswa@users.noreply.github.com>
2021-08-08 11:07:49 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,10 +8,10 @@ function preload() {
     //load the images here
     bg = loadImage("garden.png");
     tomImg1= loadAnimation("cat1.png");
-    tomImg2=loadAnimation("cat2.png","images/cat3.png");
+    tomImg2=loadAnimation("cat2.png","cat3.png");
     tomImg3= loadAnimation("cat4.png");
     mouseImg1=loadAnimation("mouse1.png");
-    mouseImg2= loadAnimation("mouse2.png","images/mouse3.png");
+    mouseImg2= loadAnimation("mouse2.png","mouse3.png");
     mouseImg3=loadAnimation("mouse4.png");
 
 }
